54dcf0c03693b54b0ae117441df8bb09abfea271
marcos <marquinhoanjos51@gmail.com>
2023-10-20 00:10:06 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+
+const relogio = setInterval(function time(){
+    let dateToday = new Date ();
+    let hr = dateToday.getHours();
+    let mn = dateToday.getMinutes();
+    let sg = dateToday.getSeconds();
+
+    /*if (hr < 10) hr = '0' + hr;
+
+    if (mn < 10) hr = '0' + mn;
+
+    if (sg < 10) hr = '0' + sg;*/
+
+    horas.textContent = hr;
+    minutos.textContent = mn;
+    segundos.textContent = sg;
+
+})
